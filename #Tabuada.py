c1b8774a6cb5fef58c88adb82ec210a454d1a1ae
VERONIKA_BOFF <veronikaboff@gmail.com>
2025-11-06 19:53:39 -0300

    primeiro commit

---
+++ #Tabuada.py
@@ -0,0 +1,8 @@
+#Tabuada 
+tabuada=int(input("digite um número "))
+for a in range (1,11):
+    print(a,"x",tabuada,"=",(a*tabuada))
+while tabuada:
+      tabuada=int(input("digite um número "))
+      for a in range (1,11):
+           print(a,"x",tabuada,"=",(a*tabuada))
